2248a25343181f745303b8cc66b321c0771849ab
nkpmedia <webmaster@nkp-media.de>
2017-10-05 21:45:05 +0200

    Fixed test Added test for reCaptcha 403

--- tests/AppBundle/Controller/FeedbackControllerTest.php
+++ tests/AppBundle/Controller/FeedbackControllerTest.php
@@ -82,6 +82,49 @@ class FeedbackControllerTest extends WebTestCase
         $this->assertEquals("200", $client->getResponse()->getStatusCode());
     }
 
+    /**
+     * @group legacy
+     */
+    public function testCreateFeedbackDatabaseEntryReCaptchaFail()
+    {
+        $reCaptcha = $this->getMockBuilder(ReCaptchaService::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $reCaptcha->expects($this->once())
+            ->method("validateReCaptcha")
+            ->willReturn(false);
+
+        $client = static::createClient();
+        $client->request('GET', '/logout');
+
+        $crawler = $client->request('GET', '/login');
+        $form = $crawler->selectButton('Login')->form();
+
+        $form['_username'] = 'TestAmbrone1';
+        $form['_password'] = 'test';
+
+        $client->submit($form);
+        $client->followRedirect();
+
+        static::$kernel->setKernelModifier(function($kernel) use ($reCaptcha) {
+            $kernel->getContainer()->set('reCaptcha', $reCaptcha);
+        });
+
+
+        $client->request("POST", "/feedback/send", array(
+            "data"=>"[{\"Text\":\"asdf\"},
+            \"picture\",
+            {\"href\":\"http://127.0.0.1:8000/\",\"ancestorOrigins\":{},\"origin\":\"http://127.0.0.1:8000\",\"protocol\":\"http:\",\"host\":\"127.0.0.1:8000\",\"hostname\":\"127.0.0.1\",\"port\":\"8000\",\"pathname\":\"/\",\"search\":\"\",\"hash\":\"\"},
+            \"browser\",
+            \"htmlText\",1506893323093,
+            \"ewfwewgerg\"]"
+        ));
+
+        var_dump($client->getResponse()->getContent());
+
+        $this->assertEquals("403", $client->getResponse()->getStatusCode());
+    }
+
     public function testCreateFeedbackDatabaseEntryNotValidData500()
     {
         $client = static::createClient();
@@ -103,6 +146,6 @@ class FeedbackControllerTest extends WebTestCase
         $this->assertContains("Object(AppBundle\Entity\UserFeedback).browserData:", $client->getResponse()->getContent());
 
 
-        $this->assertEquals("500", $client->getResponse()->getStatusCode());
+        $this->assertEquals("406", $client->getResponse()->getStatusCode());
     }
 }
