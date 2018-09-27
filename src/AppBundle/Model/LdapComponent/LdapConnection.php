fd794bcec1d2f9650e5ced27d03a911b49f36f48
Paul Kruse <paul@pbnl.de>
2018-09-27 21:32:06 +0200

    Added port and tls config option

--- src/AppBundle/Model/LdapComponent/LdapConnection.php
+++ src/AppBundle/Model/LdapComponent/LdapConnection.php
@@ -16,10 +16,12 @@ class LdapConnection
     private $use_tls;
     private $password;
     private $bind_dn;
+    private $port;
 
-    public function __construct($uri, $tls, $password, $bind)
+    public function __construct($uri, $port, $tls, $password, $bind)
     {
         $this->uri = $uri;
+        $this->port = $port;
         $this->use_tls = $tls;
         $this->password = $password;
         $this->bind_dn = $bind;
@@ -33,20 +35,30 @@ class LdapConnection
      */
     private function connect(string $server, int $port = 389)
     {
-        $this->ldapConnection = ldap_connect($server, $port);
+        if($this->use_tls) {
+            $this->ldapConnection = @ldap_connect("ldaps://$server:$port");
+        }
+        else {
+            $this->ldapConnection = @ldap_connect("ldap://$server:$port");
+        }
+
         if($this->ldapConnection == FALSE)
         {
-            throw new LdapConnectException("Cant connect to $server on port $port");
+            throw new LdapConnectException("Cant connect to $server on port $port: ".
+                ldap_error($this->ldapConnection));
         }
+
+
     }
 
     private function bind($ldapConnection, string $dn, string $password)
     {
         ldap_set_option($ldapConnection, LDAP_OPT_PROTOCOL_VERSION, 3);
-        $ldapbind = ldap_bind($ldapConnection, $dn, $password);
+        $ldapbind = @ldap_bind($ldapConnection, $dn, $password);
         if($ldapbind == FALSE)
         {
-            throw new LdapBindException("Cannot bind to server with dn $dn using password yes");
+            throw new LdapBindException("Cannot bind to server with dn $dn using password yes: ".
+                ldap_error($this->ldapConnection));
         }
         return $ldapbind;
     }
@@ -58,7 +70,7 @@ class LdapConnection
      */
     public function openConnection()
     {
-        $this->connect($this->uri);
+        $this->connect($this->uri, $this->port);
         $result = $this->bind($this->ldapConnection, $this->bind_dn, $this->password);
 
         return $result;
