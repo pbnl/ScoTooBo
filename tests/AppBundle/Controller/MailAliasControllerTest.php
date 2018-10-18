a1765f9835a7089e15a431e3cd372fe144c74640
Paul Kruse <paul@pbnl.de>
2018-10-18 22:20:31 +0200

    Added test to modify a mailAlias

--- tests/AppBundle/Controller/MailAliasControllerTest.php
+++ tests/AppBundle/Controller/MailAliasControllerTest.php
@@ -21,10 +21,29 @@ class MailAliasControllerTest extends WebTestCase
         $client = TestTools::getLoggedInStavoAmbrone();
         $crawler = $client->request("GET","/mailAlias/show/all");
 
-        $this->assertEquals("200",$client->getResponse()->getStatusCode());
+        $this->assertEquals("200", $client->getResponse()->getStatusCode());
 
         $this->assertContains("webmaster@ambronen.de", $client->getResponse()->getContent());
         $this->assertContains("wiki@pbnl.de", $client->getResponse()->getContent());
         $this->assertContains("groupWithMailingList@pbnl.de", $client->getResponse()->getContent());
+    }
+
+    public function testAddUserInNordlichtGroup()
+    {
+        $client = TestTools::getLoggedInStavoAmbrone();
+        $crawler = $client->request('GET', '/mailAlias/detail?mailAlias=nordlichter%40pbnl.de');
+
+        $form = $crawler->selectButton('Speichern')->form();
+
+        $form['form[forward][1]'] = 'test@test.de';
+
+        $client->submit($form);
+
+        $this->assertEquals("200", $client->getResponse()->getStatusCode());
+
+        $crawler = $client->request('GET', '/mailAlias/detail?mailAlias=nordlichter%40pbnl.de');
+        $respons = $client->getResponse()->getContent();
+
+        $this->assertContains('test@test.de', $respons);
     }
 }
