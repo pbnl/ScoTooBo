318cdea0b71624101ed9d0b927aaa0c90261ec58
Paul Kruse <paul@pbnl.de>
2019-06-07 00:57:43 +0200

    Updated rsync destination folder

--- docker/php-fpm/update_and_start_service.sh
+++ docker/php-fpm/update_and_start_service.sh
@@ -1,8 +1,10 @@
+sleep 5
+
 php bin/console doctrine:schema:update --force
 export SYMFONY_ENV=prod
 export APP_ENV=prod
 composer run-script post-install-cmd --no-interaction --no-dev
 
-rsync -rtvu /web-original /var/www/app/web
+rsync -rtvu /web-original/* /var/www/app/web
 
 php-fpm
