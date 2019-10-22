3f121889997582f6de66dcd4c11a25e027570c7c
Paul Kruse <paul@pbnl.de>
2019-10-22 23:23:36 +0200

    fixed-rysn-name-bug

--- docker/php-fpm/update_and_start_service.sh
+++ docker/php-fpm/update_and_start_service.sh
@@ -5,6 +5,6 @@ export SYMFONY_ENV=prod
 export APP_ENV=prod
 composer run-script post-install-cmd --no-interaction --no-dev
 
-rsync -rtvu /web-original/* /var/www/app/web
+rsync -rtvu /public-original/* /var/www/app/public
 
 php-fpm
